efe040a1609456e1726498dd90de3d91403eadfb
mansour <mansour@web-id.fr>
2020-11-17 08:20:06 +0100

    add register route

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//Import Route
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+//Connect Db
+mongoose.connect(
+    process.env.DB_CONNECT,
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    autoIndex: true,
+  },
+() => console.log('DB connected '))
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Route middlewware
+app.use('/api/user', authRoute);
+
+app.listen(5000, () => console.log('App is here'));
---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const {registerValidation} = require('../routes/validation');
+
+
+
+
+router.post('/register', async (req,res) => {
+     // Validate Data before we create user
+    const {error} = registerValidation(req.body);
+   
+    if (error) return res.status(400).send(error.details[0].message);
+
+   const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password 
+    });
+    try {
+        const savedUser = await user.save();
+        res.status(201).send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    } 
+});
+
+module.exports = router;
