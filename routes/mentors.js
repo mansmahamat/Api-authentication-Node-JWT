e4c5c377c3bcc3a6a28deeb81e4beb4482d6b375
mansour <mansour@web-id.fr>
2020-12-20 23:04:00 +0100

    find mentor by id

--- routes/mentors.js
+++ routes/mentors.js
@@ -73,36 +73,22 @@ router.get('/mentor/:id',  getOneMentors.findOne)
 router.patch('/mentors/:id', async (req, res) => {
     try {
 		const mentor = await Mentor.findOne({ _id: req.params.id })
-
-		if (req.body.firstName) {
-			mentor.firstName = req.body.firstName
-        }
-        if (req.body.lastName) {
-			mentor.lastName = req.body.lastName
-        }
-        if (req.body.avatar) {
-			mentor.avatar = req.body.avatar
-		}
-
-		if (req.body.title) {
-			mentor.title = req.body.title
-        }
-        if (req.body.disponible) {
-			mentor.disponible = req.body.disponible
-        }
-        if (req.body.presentation) {
-			mentor.presentation = req.body.presentation
-        }
-        if (req.body.technos) {
-			mentor.technos = req.body.technos
-        }
-        if (req.body.socials) {
-			mentor.socials = req.body.socials
-        }
-        if (req.body.userId) {
-			mentor.userId = req.body.userId
-		}
-
+        
+        const mentor = new Mentor({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            avatar: req.file.filename,
+            //avatar: req.body.avatar,
+            title: req.body.title,
+            disponible: req.body.disponible,
+            presentation: req.body.presentation,
+            technos: req.body.technos,
+            socials: req.body.socials,
+            userId: req.body.userId
+    
+    
+          });
+        
 		await mentor.save()
 		res.send(mentor)
 	} catch {
