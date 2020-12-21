3dcb355dbda89f682d96e5f6fbf552de72798bc9
mansour <mansour@web-id.fr>
2020-12-21 22:00:50 +0100

    find mentor by id

--- routes/mentors.js
+++ routes/mentors.js
@@ -93,9 +93,22 @@ router.patch('/mentors/:id', upload.single('avatar'),(req, res) => {
 
       });
     
-      Mentor.findByIdAndUpdate(id, req.body, req.file , { useFindAndModify: false })
-        .then(mentor => {
-          if (!mentor) {
+      Mentor.findByIdAndUpdate(id, {
+        $set: {
+          firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        avatar: req.file.filename,
+        //avatar: req.body.avatar,
+        title: req.body.title,
+        disponible: req.body.disponible,
+        presentation: req.body.presentation,
+        technos: req.body.technos,
+        socials: req.body.socials,
+        userId: req.body.userId
+        }
+      }, { useFindAndModify: false })
+        .then(result => {
+          if (!result) {
             res.status(404).send({
               message: `Ce Mentor n'existe pas`
             });
