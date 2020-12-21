825e91037145f6cbb325c11921ecbb63224649a5
mansour <mansour@web-id.fr>
2020-12-21 21:49:04 +0100

    find mentor by id

--- routes/mentors.js
+++ routes/mentors.js
@@ -93,7 +93,7 @@ router.patch('/mentors/:id', upload.single('avatar'),(req, res) => {
 
       });
     
-      Mentor.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+      Mentor.findByIdAndUpdate(id, mentor, { useFindAndModify: false })
         .then(mentor => {
           if (!mentor) {
             res.status(404).send({
