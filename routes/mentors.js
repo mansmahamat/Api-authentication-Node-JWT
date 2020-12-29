bcd8bd849d426630bf1876e5cbd7a0d208bff66f
mansour <mansour@web-id.fr>
2020-12-29 23:27:24 +0100

    error format image

--- routes/mentors.js
+++ routes/mentors.js
@@ -30,7 +30,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 2 * 1024 * 1024 * 1024 }
+  limits: { fileSize: 1024 * 1024 * 5 }
 })
 const cloudinary = require('cloudinary').v2
 cloudinary.config({
