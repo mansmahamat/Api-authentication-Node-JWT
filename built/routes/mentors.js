8d913ffd139b9104188fa89cc9a547bfd65be815
mansour <mansour@web-id.fr>
2020-12-30 16:18:22 +0100

    add method for upload file & merge preprod into prod

--- built/routes/mentors.js
+++ built/routes/mentors.js
@@ -4,12 +4,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const updateMentors_1 = __importDefault(require("../controllers/updateMentors"));
 const deleteMentors_1 = __importDefault(require("../controllers/deleteMentors"));
 const getOneMentors_1 = __importDefault(require("../controllers/getOneMentors"));
 const Mentor_1 = __importDefault(require("../models/Mentor"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const multer = require('multer');
+dotenv_1.default.config();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/jpg') {
+        cb(null, true);
+    }
+    else {
+        cb(new Error('Mauvais format'), false);
+    }
+};
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 1024 * 1024 * 5 }
+});
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
 const router = express_1.Router();
-router.post('/mentors', async (req, res) => {
+router.post('/mentors', upload.single('avatar'), async (req, res) => {
+    const result = await cloudinary.uploader.upload(req.file.path);
     const mentor = new Mentor_1.default({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -42,7 +72,37 @@ router.get('/mentors', async (req, res) => {
     }
 });
 router.get('/mentor/:id', getOneMentors_1.default);
-router.patch('/mentors/:id', updateMentors_1.default);
+router.patch('/mentors/:id', upload.single('avatar'), async (req, res) => {
+    try {
+        const id = req.params.id;
+        let mentor = await Mentor_1.default.findById(id);
+        // Delete image from cloudinary
+        //@ts-ignore
+        await cloudinary.uploader.destroy(mentor === null || mentor === void 0 ? void 0 : mentor.cloudinary_id);
+        // Upload image to cloudinary
+        const result = await cloudinary.uploader.upload(req.file.path);
+        const data = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            avatar: result.secure_url,
+            //avatar: req.body.avatar,
+            title: req.body.title,
+            disponible: req.body.disponible,
+            presentation: req.body.presentation,
+            technos: req.body.technos,
+            socials: req.body.socials,
+            userId: req.body.userId,
+        };
+        const user = await Mentor_1.default.findByIdAndUpdate(id, data, {
+            useFindAndModify: false
+        });
+        // SEND FILE TO CLOUDINARY
+        res.json(user);
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+});
 router.delete('/mentors/:id', deleteMentors_1.default);
 module.exports = router;
 //# sourceMappingURL=mentors.js.map
