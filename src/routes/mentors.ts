9a9347993e1738f3f6e3ca06d760bf20f3291b04
mansour <mansour@web-id.fr>
2021-01-05 00:22:24 +0100

    add method for upload file & merge preprod into prod

--- src/routes/mentors.ts
+++ src/routes/mentors.ts
@@ -130,7 +130,7 @@ router.patch('/mentors/:id', upload.single('avatar'),async (req:Request, res:Res
 
        res.json(user);
     } catch (err) {
-        res.status(400).send(err + "erreuuuur");
+        res.send(err + "erreuuuur");
     } 
     
     
